9cd2b90cebef778295647a2c7bed323206dab3ed
valerii.petryna <valerii.petryna@hive-studio.net>
2019-07-18 14:27:45 +0300

    add engines and npm run command

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const app = new Koa();
 const router = new Router();
 
 app.use(views(path.join(__dirname, '/src/templates'), {
-    extension: 'njk',
-    map: {
-        njk: 'nunjucks',
-    },
+  extension: 'njk',
+  map: {
+    njk: 'nunjucks',
+  },
 }));
 
-app.use(serve(require("path").join(__dirname, "/public")));
+app.use(serve(require('path').join(__dirname, '/public')));
 
 router.use('/', require('./src/routes').routes());
 
 app.use(router.routes());
 
 app.listen(3000);
+
 console.log('listening on port 3000');
